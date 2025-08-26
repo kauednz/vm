9547eb420ac868726670551ce8dba05b7df342ce
kauednz <kauednz@yahoo.com>
2025-08-26 13:45:24 -0300

    Add files via upload

---
+++ src/app.jsx.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VentureMCloudPlatform from './components/VentureMCloudPlatform';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <VentureMCloudPlatform />
+    </div>
+  );
+}
+
+export default App;
